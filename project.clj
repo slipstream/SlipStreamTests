204fb9f2ec7169ab17b7103cba36ceb7afe3f723
sixsq-hudson <hudson@sixsq.com>
2018-08-17 15:19:04 +0000

    next development version

--- project.clj
+++ project.clj
@@ -1,6 +1,6 @@
-(def +version+ "3.57")
+(def +version+ "3.58-SNAPSHOT")
 
-(defproject com.sixsq.slipstream/SlipStreamTests-clj "3.57"
+(defproject com.sixsq.slipstream/SlipStreamTests-clj "3.58-SNAPSHOT"
 
   :description "SlipStream functional tests"
 
