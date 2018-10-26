7638248d4914fa8cb9fab86f7132185da724a79a
sixsq-hudson <hudson@sixsq.com>
2018-10-26 14:44:26 +0000

    release v3.62

--- project.clj
+++ project.clj
@@ -1,6 +1,6 @@
-(def +version+ "3.62-SNAPSHOT")
+(def +version+ "3.62")
 
-(defproject com.sixsq.slipstream/SlipStreamTests-clj "3.62-SNAPSHOT"
+(defproject com.sixsq.slipstream/SlipStreamTests-clj "3.62"
 
   :description "SlipStream functional tests"
 
