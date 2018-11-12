497fcf8062bcb9e2c382487f409203edeedcb15d
Charles Loomis <cal@sixsq.com>
2018-11-12 09:25:59 +0100

    update parent

--- project.clj
+++ project.clj
@@ -13,7 +13,7 @@
   :plugins [[lein-parent "0.3.2"]
             [test2junit "1.4.0"]]
 
-  :parent-project {:coords  [sixsq/slipstream-parent "5.3.13"]
+  :parent-project {:coords  [sixsq/slipstream-parent "5.3.14"]
                    :inherit [:plugins
                              :min-lein-version
                              :managed-dependencies
