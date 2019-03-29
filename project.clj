668478a9780e9cec6d82eebae5b1fabd4af86fe8
sixsq-hudson <hudson@sixsq.com>
2019-03-29 11:04:36 +0000

    release v3.71

--- project.clj
+++ project.clj
@@ -1,6 +1,6 @@
-(def +version+ "3.71-SNAPSHOT")
+(def +version+ "3.71")
 
-(defproject com.sixsq.slipstream/SlipStreamTests-clj "3.71-SNAPSHOT"
+(defproject com.sixsq.slipstream/SlipStreamTests-clj "3.71"
 
   :description "SlipStream functional tests"
 
